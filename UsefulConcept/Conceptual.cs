8885e5ef7599d875f21d17f6665fc195fd519e02
Mahendrata Harpi <justharpi@gmail.com>
2023-01-26 12:43:52 +0700

    conceptual

---
+++ UsefulConcept/Conceptual.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulConcept
+{
+    internal class Conceptual
+    {
+        class Shape { }
+        class Circle : Shape { }
+        class Rectangle : Shape { }
+        public static void IsOperator()
+        {
+            Circle circle = new Circle();
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine($"Is Circle inherit from Shape: {circle is Shape}");
+            Console.WriteLine($"Is Rectangle inherit from Shape: {rectangle is Shape}");
+        }
+
+        public static void AsOperator()
+        {
+            Shape shape = new();
+            Circle circle = new();
+
+            Shape? convertToShape = circle as Shape;
+
+            Console.WriteLine($"convertions 'circle as Shape' produces {convertToShape}");
+
+            Circle? convertCircle = shape as Circle;
+
+            if (convertCircle == null)
+            {
+                Console.WriteLine($"Conversion 'shape as Circle' produces null");
+            }
+            else
+            {
+                Console.WriteLine($"Conversion 'shape as Circle' produces {convertCircle}");
+            }
+        }
+
+        public static void ShowNullable()
+        {
+            // variable reference type
+            string? name = "jojon";
+
+            if (name == null) Console.WriteLine("null value");
+
+            // variable value type 
+            int? age = null;
+            if (age.HasValue)
+            {
+                Console.WriteLine($"age : {age}");
+            }
+            else
+            {
+                Console.WriteLine("age has no value");
+            }
+        }
+
+        public static void PassingByValue(int x)
+        {
+            x *= 2;
+            Console.WriteLine($"Body PassingByVaue, x value : {x}");
+        }
+
+        public static void PassingByReference(ref int x)
+        {
+            x *= 3;
+            Console.WriteLine($"Body PassingByReference, y value : {x}");
+        }
+    }
+
+    internal class Employee
+    {
+        public const decimal baseSalary = 100_000;
+        public readonly int _id = 100;
+
+        public Employee(int id)
+        {
+            _id = id;
+        }
+
+        // cannot set readOnly field (readonly field cannot be assigned)
+        //public int SetId(int id)
+        //{
+        //    return _id = id;
+        //}
+
+        // readonly property
+        public int Age { get; } = 20;
+
+        public readonly List<int> myNums = new();
+
+        public void AddNum(int num)
+        {
+            myNums.Add(num);
+        }
+
+        public void DisplayNums()
+        {
+            foreach (var item in myNums)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        public static void UpdatePromo(double price, float promo,
+            out decimal totalPrice, out decimal totalDiscount)
+        {
+            totalPrice = (decimal)((price * (promo/ 100)) + price);
+            totalDiscount = (decimal)(price - promo);
+        }
+    }
+}
---
+++ UsefulConcept/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace UsefulConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp = new Employee(101);
+            Console.WriteLine($"{emp._id}");
+            Console.WriteLine($"{emp.Age}");
+
+            emp.AddNum(10);
+            emp.AddNum(11);
+            emp.AddNum(12);
+            emp.DisplayNums();
+
+            decimal totalPrice, totalDiscount;
+            Employee.UpdatePromo(50_000, 1000, out totalPrice, out totalDiscount);
+            //Console.WriteLine($"Total Price: {totalPrice}");
+            //Console.WriteLine($"Total Discount: {totalDiscount}");
+
+            //Conceptual.IsOperator();
+
+            //Conceptual.AsOperator();
+
+            int x = 10; // immutable
+            //Conceptual.PassingByValue(x);
+            //Console.WriteLine($"Final value x : {x}");
+
+            int y = 10; // mutable
+            //Conceptual.PassingByReference(ref y);
+            //Console.WriteLine($"Final value y : {y}");
+
+            //Conceptual.ShowNullable();
+        }
+    }
+}
