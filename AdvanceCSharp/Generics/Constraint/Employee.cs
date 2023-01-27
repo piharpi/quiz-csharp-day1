3b8af304f08176d5d6bf4439c0173fb3396352d1
Mahendrata Harpi <justharpi@gmail.com>
2023-01-27 19:03:39 +0700

    advance generic constraint

---
+++ AdvanceCSharp/Generics/Constraint/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceCSharp.Generics.Constraint
+{
+    internal interface IEmployee
+    {
+    }
+
+    internal class Employee : IEmployee
+    {
+        public Employee(string? name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string? ToString()
+        {
+            return $"Name : {Name}, Salary: {Salary}";
+        }
+
+        public string? Name { get; set; }
+        public double Salary { get; set; }
+    }
+
+    internal class EmployeeRepository<T> where T : IEmployee
+    {
+        readonly List<Employee> listOfEmployees = new();
+
+        public void AddEmployee(Employee emp)
+        {
+            listOfEmployees.Add(emp);
+        }
+
+        public void DisplayEmployees()
+        {
+            Console.WriteLine("Display Employee : ");
+
+            foreach (var emp in listOfEmployees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+        }
+    } 
+
+    internal class Person
+    {
+        public Person(string? name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string? ToString()
+        {
+            return $"Name : {Name}, Salary: {Salary}";
+        }
+        public string? Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
--- AdvanceCSharp/Program.cs
+++ AdvanceCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdvanceCSharp.Generics;
+using AdvanceCSharp.Generics.Constraint;
 using AdvanceCSharp.Generics.Interfaces;
 
 namespace AdvanceCSharp
@@ -7,21 +8,37 @@ namespace AdvanceCSharp
     {
         static void Main(string[] args)
         {
+            // generic constraint
+            Employee emp1 = new("Dobleh", 10_000_000);
+            Employee emp2 = new("Joko", 15_000_000);
+            Employee emp3 = new("Kabor", 12_000_000);
+
+            // declare employee repo
+            EmployeeRepository<Employee> listEmployees = new EmployeeRepository<Employee>();
+
+            listEmployees.AddEmployee(emp1);
+            listEmployees.AddEmployee(emp2);        
+            listEmployees.AddEmployee(emp3);
+
+            listEmployees.DisplayEmployees();
+
+            EmployeeRepository<Person> listPerson = new EmployeeRepository<Person>();
+
             // call interface
             IVehicle<string> vehicle = new Car<string>();
             string color = vehicle.GetColor("Black Metallic");
             vehicle.InfoVehicle();
-            Console.WriteLine($"{color}");
+            //Console.WriteLine($"{color}");
 
             // call person generic
             Person<string> personString = new();
-            Console.WriteLine($"Person string : {personString.DisplayInfo("Hello")}");
+            //Console.WriteLine($"Person string : {personString.DisplayInfo("Hello")}");
 
             Person<int> personInt= new();
-            Console.WriteLine($"Person int : {personInt.DisplayInfo(1945)}");
+            //Console.WriteLine($"Person int : {personInt.DisplayInfo(1945)}");
 
             Person<double> personDouble = new();
-            Console.WriteLine($"Person double : {personDouble.DisplayInfo(3.00)}");
+            //Console.WriteLine($"Person double : {personDouble.DisplayInfo(3.00)}");
 
             // sample list generic
             MyList.DisplayListString();
